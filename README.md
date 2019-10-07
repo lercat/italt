a544249d9d62f1f86be285c1f5df8843021e50b2
lercat <ddbieres@gmail.com>
2019-10-07 22:05:28 +0200

    modif read me

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # italt
-blabla italt
+italt : un site dédié à l'alternance pour des candidats en recherche d'entreprises ou d'organismes de formation.
+Pour permettre à des entreprises de poster des offres et de consulter des cv.
---
+++ italtequipe/resources/views/accueil.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo url('/assets/css/style.css') ?>">
+    <title>MON SITE MARKETPLACE</title>
+</head>
+<body>
+<header>
+        <h1>Mon site marketPlace</h1>
+        <nav>
+            <ul>
+                <li><a href="<?php echo url('/') ?>">Accueil</a></li>
+                <li><a href="<?php echo url('/galerie') ?>">Galerie</a></li>
+                <li><a href="<?php echo url('/contact') ?>">contact</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <section>
+            <h3>Ma section accueil</h3>
+        </section>
+    </main>
+    <footer>
+        <p>Tous droits réservés  @Dbieres 2019</p>
+    </footer>
+
+
+    <script src="<?php echo url('/assets/js/main.js') ?>"></script>
+
+</body>
+</html>
