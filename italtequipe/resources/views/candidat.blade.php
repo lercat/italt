a16fbf09abae1b53926da3650a7f844a46400590
lercat <ddbieres@gmail.com>
2019-10-10 10:01:22 +0200

    pull 10/10

--- italtequipe/resources/views/candidat.blade.php
+++ italtequipe/resources/views/candidat.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+jjj<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -7,14 +7,38 @@
     <title>Candidat</title>
 </head>
 <body>
-<h1>candidat</h1>
+<header>
         <nav>
-            <ul>
+        <div>
+            <a href='#'><img src="assets/logo.jpg"> </a>
+        </div>
+            <ul id="menu_nav">
                 <li><a href="<?php echo url('/candidat') ?>">Candidat</a></li>
                 <li><a href="<?php echo url('/entreprise') ?>">Entreprise</a></li>
                 <li><a href="<?php echo url('/ecole') ?>">Organisme de Formation</a></li>
             </ul>
         </nav>
-    
+        <h1>candidat</h1>
+ </header>
+ <main>
+    <section> Section Formulaire de recherche</section>
+    <section> Catégorie les plus consultées</section>
+    <section>Qui sommes nous</section>
+    <section>Partenaires</section>
+    <footer>
+        <div>
+
+        </div>
+        <nav>
+            <ul id="menu_options">
+            <a href="#"><li>Twitter</li></a>
+            <a href="#"><li>Facebook</li></a>
+            <a href="#"><li>Linkedin</li></a>
+            <a href="#"><li>FAQ</li></a>
+            </ul>
+        </nav>
+    </footer>
+</main>
+
 </body>
 </html>
