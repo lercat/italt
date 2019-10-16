410a4bb9a845440c01d46cd0b28502f34119e50d
lercat <ddbieres@gmail.com>
2019-10-16 15:07:46 +0200

    ajout img fond carte

--- italtequipe/resources/views/accueil.blade.php
+++ italtequipe/resources/views/accueil.blade.php
@@ -1,53 +1,74 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?php echo url('/assets/css/style-accueil.css') ?>">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.css" integrity="sha256-PF6MatZtiJ8/c9O9HQ8uSUXr++R9KBYu4gbNG5511WE=" crossorigin="anonymous" />
-    <title>IT Alternance</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="<?php echo url('/assets/css/style-accueil.css') ?>">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.css" integrity="sha256-PF6MatZtiJ8/c9O9HQ8uSUXr++R9KBYu4gbNG5511WE=" crossorigin="anonymous" />
+  <title>IT Alternance</title>
 </head>
+
 <body>
-        <div class="header">
-                <img src="assets/images/logo_italt_aseul.png" class="logo" alt="log" >
-                <div class="header-right">
-                        <a class="active" a href="<?php echo url('/ecole') ?>">Écoles</a>
-                        <a class="active1" a href="<?php echo url('/candidat') ?>">Candidats</a>
-                        <a class="active2" a href="<?php echo url('/entreprise') ?>">Entreprises</a>
-                        <a class="active1" a href="<?php echo url('/contact') ?>">Contact</a>
-                        <a href="#"><i class="fa fa-user 7x"></i></a>
-                </div>
-              </div>
+  <div class="header">
+    <img src="assets/images/logo_italt_aseul.png" class="logo" alt="log">
+    <div class="header-right">
+      <a class="active" a href="<?php echo url('/ecole') ?>">Écoles</a>
+      <a class="active1" a href="<?php echo url('/candidat') ?>">Candidats</a>
+      <a class="active2" a href="<?php echo url('/entreprise') ?>">Entreprises</a>
+      <a class="active1" a href="<?php echo url('/contact') ?>">Contact</a>
+      <a href="#"><i class="fa fa-user 7x"></i></a>
+    </div>
+  </div>
 
 
-             <section id="a-propos">
-              <div>
-                  <img src="assets/images/header.png" width="100%" >
-                <h1>Que pouvons nous faire pour vous ?</h1>
-                <p>Cliquez sur la rubrique correspondant à votre profil<br>pour découvrir ce que nous proposons</p>
-                </div>
-          </section>  
-              <br><br>
-          
-              
-                <div id="items">
-                <a class="actif" href="#écoles">Vous êtes une Ecole</a>
-                <a class="actif1" href="#candidats">Vous êtes un Alternant</a>
-                <a class="actif2" href="#entreprises">Vous êtes une Entreprise</a>
-            <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem 
-                accusantium doloremque laudantium, totam rem aperiam, 
-                eaque ipsa quae ab illo inventore veritatis et quasi architecto 
-                beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia v
-                oluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni
-                 dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est,
-                  qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia 
-                  non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat 
-                  voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis 
-                  suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel 
-                  eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, 
-                  vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
-            </p>
-            </div>
-            
+  <section id="a-propos">
+    <div>
+      <img src="assets/images/header.png" width="100%">
+      <h1>It-Alt est un site dédié à l'alternance. </h1>
+      <h2>Que pouvons nous faire pour vous ?</h2>
+      <p>Cliquez sur la rubrique correspondant à votre profil<br>pour découvrir ce que nous proposons</p>
+    </div>
+  </section>
+  <br><br>
+
+
+  <div id="items">
+    <a class="actif" href="#écoles">Vous êtes une École</a>
+    <a class="actif1" href="#candidats">Vous êtes un(e) Alternant(e)</a>
+    <a class="actif2" href="#entreprises">Vous êtes une Entreprise</a>
+    <p>Pour vous candidats en recherche d'entreprises et/ou d'organismes de formation, vous trouverez ici
+      des offres, des informations sur les questions que vous vous posez.</p>
+    <p>Pour vous, Entreprises publiez vos offres, consultez les cv des candidats (& leurs travaux réalisés).</p>
+    <p> Les écoles de formation pourront indiquer les programmes des sessions qu'ils proposent ainsi que tous les
+      renseignements necessaires. Ils auront un accès à la cvthèque.</p>
+  </div>
+
+  <section>
+    <h2>Notre équipe</h2>
+    <p>Quatre apprenantes en formation, dont certaines veulent partir en alternance pour compléter leur savoir-faire en développement web.
+      Nous comptons sur tout le monde pour que ce site émerge et devienne réalité !!!</p>
+  </section>
+
+
+  <section>
+    <h2>Ils nous soutiennent</h2>
+  </section>
+
+  <section>
+    <h2>Passez à l'action</h2>
+    <button type="submit">S'inscrire</button>
+    <p>Une question ? Un problème ?</p>
+    <p>FAQ</p>
+    <p>Nous contacter</p>
+
+  </section>
+  </main>
+
+
+<footer>
+
+</footer>
 </body>
+
 </html>
