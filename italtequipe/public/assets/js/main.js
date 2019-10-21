1796dc0ac7fdb1c8ae01c15da43cb9330e05244e
fazia <f.o.bouheraoua@gmail.com>
2019-10-21 15:14:47 +0200

    changement page d'accueil

--- italtequipe/public/assets/js/main.js
+++ italtequipe/public/assets/js/main.js
@@ -1,25 +1,139 @@
-// Get the modal
-var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+//------------------------ NAVBAR -----------------------
+//-------------------------------------------------------
+$(document).ready(function () {
+	$('a[href^="#"]').on('click', function (evt) {
+		var target = $(this).attr('href');
+		$('html, body')
+			.animate({
+				scrollTop: $(target).offset().top
+			}, 600);
+	});
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+	$(".menu-icon").on("click", function () {
+		$(".burger").toggleClass("active");
+		$("nav ul").toggleClass("showing");
+		$("nav").addClass("background-scroll");
+	});
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
+	$("nav a").on("click", function () {
+		$(".burger").removeClass("active");
+		$("nav ul").removeClass("showing");
+	});
+});
+
+$(window).scroll(function () {
+	if ($(window).scrollTop() > 1) {
+		$('nav').addClass("background-scroll");
+	} else {
+		$('nav').removeClass("background-scroll");
+		$("nav ul").removeClass("showing");
+		$(".burger").removeClass("active");
+	}
+});
+
+//------------------ Effet Parallax ----------------
+//--------------------------------------------------
+const parallax = document.querySelector("#accueil");
+
+window.addEventListener('scroll', () => {
+	setTimeout(moveBackground, 1)
+});
+
+function moveBackground() {
+	parallax.style.backgroundPositionY = window.scrollY / 5 + "px";
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+//--------------- ScrollReveal ---------------------
+//--------------------------------------------------
+const sr = ScrollReveal();
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+sr.reveal('.logoa', {
+	origin: 'top',
+	distance: '200px',
+	duration: 3000,
+});
+
+sr.reveal('.logo1', {
+	origin: 'left',
+	distance: '200px',
+	duration: 5000,
+});
+
+sr.reveal('h2', {
+	origin: 'right',
+	distance: '200px',
+	duration: 1500,
+});
+
+sr.reveal('.social-overlay', {
+	origin: 'bottom',
+	distance: '250px',
+	duration: 1500,
+});
+
+sr.reveal('.scroll-overlay', {
+	origin: 'top',
+	distance: '200px',
+	duration: 1500,
+	delay: 800
+});
+
+sr.reveal('.picture', {
+	origin: 'top',
+	distance: '150px',
+	duration: 2000,
+	mobile: false
+});
+
+sr.reveal('figure', {
+	duration: 3000,
+	interval: 250,
+	origin: 'bottom',
+	distance: '50px',
+});
+sr.reveal('.figure-img1',{
+	duration: 3000,
+	interval: 250,
+	origin: 'bottom',
+	distance: '50px',
+});
+//------------------------ BOUTTON SCROLL TELEPHONE -----------------------
+//-------------------------------------------------------
+
+
+var mybutton = document.getElementById("myBtn");
+
+// Quand l'utilisateur scroll down de 20px , montrer le boutton
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
   }
 }
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+
+
+
+// //------------------------ CHANGEMENT CARTE AU CLIQUE -----------------------
+// //-------------------------------------------------------
+
+
+function changeImage(element) {
+    document.getElementById('imageReplace').src = element;
+    }
+
+
+
+
+
--- italtequipe/resources/views/accueil.blade.php
+++ italtequipe/resources/views/accueil.blade.php
@@ -1,74 +1,146 @@
 <!DOCTYPE html>
 <html lang="fr">
-
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="<?php echo url('/assets/css/style-accueil.css') ?>">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.css" integrity="sha256-PF6MatZtiJ8/c9O9HQ8uSUXr++R9KBYu4gbNG5511WE=" crossorigin="anonymous" />
-  <title>IT Alternance</title>
+    <meta charset="UTF-8">
+    <meta name="description" content="Alternance pour le numérique" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo url('/assets/css/style-accueil.css') ?>">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+   <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet"> 
+    <script src="assets/js/scrollreveal.min.js"></script>
+    <title>IT ALTERNANCE au service du numérique</title>
 </head>
-
 <body>
-  <div class="header">
-    <img src="assets/images/logo_italt_aseul.png" class="logo" alt="log">
-    <div class="header-right">
-      <a class="active" a href="<?php echo url('/ecole') ?>">Écoles</a>
-      <a class="active1" a href="<?php echo url('/candidat') ?>">Candidats</a>
-      <a class="active2" a href="<?php echo url('/entreprise') ?>">Entreprises</a>
-      <a class="active1" a href="<?php echo url('/contact') ?>">Contact</a>
-      <a href="#"><i class="fa fa-user 7x"></i></a>
-    </div>
-  </div>
+       
+    
 
 
-  <section id="a-propos">
-    <div>
-      <img src="assets/images/header.png" width="100%">
-      <h1>It-Alt est un site dédié à l'alternance. </h1>
-      <h2>Que pouvons nous faire pour vous ?</h2>
-      <p>Cliquez sur la rubrique correspondant à votre profil<br>pour découvrir ce que nous proposons</p>
-    </div>
-  </section>
-  <br><br>
+    <nav>
+        <div class="menu-icon">
+                <img src="assets/images/logoa.png" a href="#accueil" class="logo2" alt="logo" > 
+            <div class="burger">
+                     
+                   
+                <span></span>
+            </div>
+        </div>
+
+        <div class="menu">
+            <ul>
+                    <img src="assets/images/logoa.png" class="logoa" alt="logo"> 
+                     <li><a class="active"  a href="<?php echo url('/ecole') ?>">Écoles</a></li>
+                     <li><a class="active1" a href="<?php echo url('/candidat') ?>">Candidats</a></li>
+                     <li><a class="active2" a href="<?php echo url('/entreprise') ?>">Entreprises</a></li>
+                    <li><a class="active3"  a href="<?php echo url('/contact') ?>">Contact</a></li>
+                     <li><a href="#"><i class="fa fa-user 7x"></i></a></li>
+                     
+         </ul>
+         </div>
+       
+    </nav>
+      
+      <a id="myBtn" href="#accueil"><i class="fa fa-chevron-up" aria-hidden="true"></i></a>
+    
 
 
-  <div id="items">
-    <a class="actif" href="#écoles">Vous êtes une École</a>
-    <a class="actif1" href="#candidats">Vous êtes un(e) Alternant(e)</a>
-    <a class="actif2" href="#entreprises">Vous êtes une Entreprise</a>
-    <p>Pour vous candidats en recherche d'entreprises et/ou d'organismes de formation, vous trouverez ici
-      des offres, des informations sur les questions que vous vous posez.</p>
-    <p>Pour vous, Entreprises publiez vos offres, consultez les cv des candidats (& leurs travaux réalisés).</p>
-    <p> Les écoles de formation pourront indiquer les programmes des sessions qu'ils proposent ainsi que tous les
-      renseignements necessaires. Ils auront un accès à la cvthèque.</p>
-  </div>
+      
+    <header id="accueil">
+        <div class="overlay">
+          <!-- <h1><span></span></h1> -->
+         <img src="assets/images/logo_italt_sombre.png" a href="#accueil" class="logo1" alt="logo">
+         <h2>LE PREMIER PAS VERS<br>L'ALTERNANCE</h2>
 
-  <section>
-    <h2>Notre équipe</h2>
-    <p>Quatre apprenantes en formation, dont certaines veulent partir en alternance pour compléter leur savoir-faire en développement web.
-      Nous comptons sur tout le monde pour que ce site émerge et devienne réalité !!!</p>
-  </section>
+            <div class="scroll-overlay">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+    </header>
 
-
-  <section>
-    <h2>Ils nous soutiennent</h2>
-  </section>
-
-  <section>
-    <h2>Passez à l'action</h2>
-    <button type="submit">S'inscrire</button>
-    <p>Une question ? Un problème ?</p>
-    <p>FAQ</p>
-    <p>Nous contacter</p>
-
-  </section>
-  </main>
+    
+    
+    <section id="a-propos">
+        <div class="title">
+             <h3><span>Que pouvons nous faire pour vous ?</span></h3>
+            <p>Cliquez sur la rubrique correspondant à votre profil<br>pour découvrir ce que nous proposons</p> 
+        </div>
 
 
-<footer>
+            
+            <ul class="nav1">
+            <img src="" alt="Images" id="imageReplace"/><br />
+           <a href="#a-propos" onclick="changeImage('assets/images/fond-card-entreprise.png');">Vous êtes une Entreprise</a>
+           <a href="#a-propos" onclick="changeImage('assets/images/fond-card-alternant.png');">Vous êtes un Candidat</a>
+         <a href="#a-propos" onclick="changeImage('assets/images/fond-card-ecole.png');">Vous êtes une École</a>
+        </ul>
+           
+    </section>
 
-</footer>
+    <section id="portfolio">
+            <div class="Qui">
+                     
+                    <img src="assets/images/qui.png" class=="naz" alt="vecteur2" width="1250" height="500">
+                            
+                                <div class="center"></div>
+                                Qui sommes-nous ?
+                        <br>Nous sommes une équipe de developpeuses web FullStack , qui ont pour ambition de rendre 
+                        plus facile l'accès à l'alternace dans le domaine du numérique.C'est pourquoi la création de cette plateforme qui regrouperait les 3 parties et leurs besoins (Alternant,Entreprise,Ecole)
+                         , nous paraissait évidente.
+                            </div>
+                     </div>
+                  </div>
+
+        <!-- <div class="portfolio-container">
+           
+            <figure>
+                <div class="figure-img">
+                    <!-- <img src="img/qui.png" alt="vecteur1"> -->
+                <!-- </div>
+                <figcaption>
+                    <div class="figcaption-details">
+                       <!-- <h4></h4> -->
+                        <p></p>
+                    </div>
+                </figcaption>
+            </figure>
+        </div> 
+         </section>
+
+    <section id="contact"> 
+            <h3 class="insta">Ils nous soutiennent </h3>
+              <div class="figure-img1">
+               <a href=""><img src="assets/images/logo-part/logo-marseille.png" alt="instapic1"></a>
+               <a href=""><img src="assets/images/logo-part/logo-french-tech.png" alt="instapic2"></a>
+               <a href=""><img src="assets/images/logo-part/logo-simplon.png" alt="instapic3"></a>
+              </div>
+            
+    </section>
+
+   
+            <img src="assets/images/orange.png" alt="" width="1000" height="100"></div>   
+
+
+    <footer>
+            
+        <div class="footer-container">
+                
+            <a href="ml.html" target="_blank"></a>
+            <a href="licence.html" target="_blank"></a>
+            <!-- <div class="blockContact1">
+            <p class="blockContact">
+                    Téléphone : +33 7 82 19 76 43
+                    Email : <a href= "mailto:ITALT@gmail.com">Email</a>
+                    Facebook : <a href="">Facebook</a>
+                    Instagram : <a href="">Instagram</a>
+                    </p> -->
+                </div>
+        </div>
+    </footer>
+    
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/sweetalert2.all.min.js"></script>
+    <script src="<?php echo url('/assets/js/main.js') ?>"></script>
+    
 </body>
-
 </html>
