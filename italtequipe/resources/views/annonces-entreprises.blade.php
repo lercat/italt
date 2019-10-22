e4131e88a948976fc558aea52906c752f0cda605
lercat <ddbieres@gmail.com>
2019-10-22 16:32:37 +0200

    login-register

---
+++ italtequipe/resources/views/annonces-entreprises.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Annonces</title>
+</head>
+
+<body>
+    <header>
+        <h1>Page des annonces</h1>
+    </header>
+    <main>
+        <section>
+            <h3>LISTE DES ANNONCES</h3>
+<?php
+// on affiche les annonces avec php
+// ds 
+
+
+?>
+        </section>
+    </main>
+    <footer>
+        <p>Tous droits réservés - ©italt - 2019</p>
+    </footer>
+</body>
+</html>
