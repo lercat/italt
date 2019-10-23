6e3455dade1f28ad5a9b27cb1b9ab566fca7a5ea
lercat <ddbieres@gmail.com>
2019-10-23 23:41:24 +0200

    message formulaire en français

--- italtequipe/resources/views/layouts/app.blade.php
+++ italtequipe/resources/views/layouts/app.blade.php
@@ -43,7 +43,7 @@
                             </li>
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('inscription') }}</a>
                                 </li>
                             @endif
                         @else
