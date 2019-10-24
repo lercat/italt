f4c370da89dd45f1fa50277b011ee2c38eb3e510
fazia <f.o.bouheraoua@gmail.com>
2019-10-24 14:08:34 +0200

    js probleme réglé

--- italtequipe/resources/views/accueil.blade.php
+++ italtequipe/resources/views/accueil.blade.php
@@ -8,7 +8,7 @@
         <link rel="stylesheet" href="<?php echo url('/assets/css/style-accueil.css') ?>">
        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
        <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet"> 
-        <script src="assets/js/scrollreveal.min.js"></script>
+        <script src="<?php echo url('/assets/js/scrollreveal.min.js')?>"</script>
         <title>IT ALTERNANCE au service du numérique</title>
 </head>
 
