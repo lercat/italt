2985b595789bc0f151fd78244ab905cb531f4a47
lercat <ddbieres@gmail.com>
2019-11-04 15:22:48 +0100

    message en français

--- italtequipe/resources/views/auth/passwords/email.blade.php
+++ italtequipe/resources/views/auth/passwords/email.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+                <div class="card-header">{{ __('Réinitialiser le mot de passe') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -34,7 +34,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
+                                    {{ __('Envoyer le lien de réinitialisation du mot de passe') }}
                                 </button>
                             </div>
                         </div>
