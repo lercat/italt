8e9eeadc346bc92eb7e78078a9da75da841f6c42
fazia <f.o.bouheraoua@gmail.com>
2019-11-24 15:15:16 +0100

    changements css

--- italtequipe/resources/views/candidat.blade.php
+++ italtequipe/resources/views/candidat.blade.php
@@ -1,33 +1,38 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="<?php echo url('/assets/css/style-candidat.css') ?>">
 
-    <title>Candidat</title>
+<head>
+        <meta charset="UTF-8">
+        <meta name="description" content="Alternance pour le numérique" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="<?php echo url('/assets/css/style-candidat.css') ?>">
+       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+       <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet"> 
+        <script src="<?php echo url('/assets/js/scrollreveal.min.js') ?>"></script>
+        <title>IT ALTERNANCE au service du numérique</title>
 </head>
+
 <body>
 
 
-<header>
 
-<nav>
+
+    <nav>
         <div class="menu-icon">
            <a href="<?php echo url('/accueil') ?>"><img src="assets/images/logoa.png" class="logo2" alt="logo"></a> 
             <div class="burger">
+            <span></span>
 
             </div>
         </div>
 
-        <div >
-            <ul class="menu">
+        <div class="menu">
+            <ul>
                 <img src="assets/images/logoa.png" class="logoa" alt="logo">
-                <li><a class="active"  href="<?php echo url('/ecole') ?>">Écoles</a></li>
-                <li><a class="active1"  href="<?php echo url('/candidat') ?>">Candidats</a></li>
-                <li><a class="active2"  href="<?php echo url('/entreprise') ?>">Entreprises</a></li>
-                <li><a class="active3"  href="<?php echo url('/contact') ?>">Contact</a></li>
+                <li><a class="active" a href="<?php echo url('/ecole') ?>">Écoles</a></li>
+                <li><a class="active1" a href="<?php echo url('/candidat') ?>">Candidats</a></li>
+                <li><a class="active2" a href="<?php echo url('/entreprise') ?>">Entreprises</a></li>
+                <li><a class="active3" a href="<?php echo url('/contact') ?>">Contact</a></li>
                 <li><a href="<?php echo url('/home') ?>"><i class="fa fa-user 7x" id="icon"></i></a></li>
 
             </ul>
@@ -37,36 +42,69 @@
 
     <a id="myBtn" href="#accueil"><i class="fa fa-chevron-up" aria-hidden="true"></i></a>
 
+
+
+
     <header id="accueil">
         <div class="overlay">
             <!-- <h1><span></span></h1> -->
-            <img src="assets/images/logo_italt_sombre.png"  class="logo1" alt="logo">
-            <h2>Alternant<br></h2>
+            <img src="assets/images/logo_italt_sombre.png" a href="#accueil" class="logo1" alt="logo">
+            <h2><br>ALTERNANT</h2>
 
     </header>
 
 
 
+    
+        <div class="title">
+            <h3><span>Rechercher par catégories</span></h3>
+            
+        </div>
 
-        
 
-        <h1>candidat</h1>
- </header>
- <main>
-    <section> Section Formulaire de recherche</section>
-    <section> Catégorie les plus consultées</section>
-    <section>Partenaires</section>
+
+
+
+
+
+    <section id="contact">
+        <h3 class="insta">Ils nous soutiennent </h3>
+        <div class="figure-img1">
+            <a href=""><img src="assets/images/logo-part/logo-simplon.png" alt="instapic3"></a>
+            <a href=""><img src="assets/images/logo-part/logo-french-tech.png" alt="instapic2"></a>
+            <a href=""><img src="assets/images/logo-part/logo-marseille.png" alt="instapic1"></a>
+        </div>
+        <div class="Quii">
+            <img src="assets/images/orange.png" class="bandeorange" alt="vecteur2">
+            <div class="center1">
+                    <p class="action">Passez à l'action !</p>
+                    <button class="btn2" onclick=window.location.href='http://localhost/italt/italtequipe/public/register'>S'inscrire</button>
+                   </div>
+               </div>
+    </section>
+
+
     <footer>
-        
-        <nav>
-            <ul id="menu_footer">
-            <a href="#"><li>Twitter</li></a>
-            <a href="#"><li>Facebook</li></a>
-            <a href="#"><li>Linkedin</li></a>
-            </ul>
-        </nav>
-    </footer>
-</main>
+        <div class="footer-container">
 
+            <a href="ml.html" target="_blank"></a>
+            <a href="licence.html" target="_blank"></a>
+           <!-- <p>Tous droits réservés - ©italt - 2019</p>-->
+
+            <!-- <div class="blockContact1">
+            <p class="blockContact">
+                    Téléphone : +33 7 82 19 76 43
+                    Email : <a href= "mailto:ITALT@gmail.com">Email</a>
+                    Facebook : <a href="">Facebook</a>
+                    Instagram : <a href="">Instagram</a>
+                    </p> -->
+        </div>
+        </div>
+    </footer>
+
+    <script src="<?php echo url('/assets/js/jquery.min.js') ?>"></script>
+    <script src="<?php echo url('/assets/js/sweetalert2.all.min.js') ?>"></script>
+    <script src="<?php echo url('/assets/js/main.js') ?>"></script>
 </body>
+
 </html>
--- italtequipe/resources/views/contact.blade.php
+++ italtequipe/resources/views/contact.blade.php
@@ -1,153 +1,63 @@
-<!DOCTYPE html>
-<html lang="fr">
-
 <head>
-        <meta charset="UTF-8">
-        <meta name="description" content="Alternance pour le numérique" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="<?php echo url('/assets/css/style-candidat.css') ?>">
-       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-       <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet"> 
-        <script src="<?php echo url('/assets/js/scrollreveal.min.js') ?>"></script>
-        <title>IT ALTERNANCE au service du numérique</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="<?php echo url('/assets/css/style-contact.css') ?>">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.css" integrity="sha256-PF6MatZtiJ8/c9O9HQ8uSUXr++R9KBYu4gbNG5511WE=" crossorigin="anonymous" />
+  <title>IT Alternance</title>
 </head>
 
 <body>
+  <div class="header">
+    <img src="assets/images/logo_italt_aseul.png" class="logo" alt="log">
+    <div class="header-right">
+      <a class="active" a href="<?php echo url('/ecole') ?>">Écoles</a>
+      <a class="active1" a href="<?php echo url('/candidat') ?>">Candidats</a>
+      <a class="active2" a href="<?php echo url('/entreprise') ?>">Entreprises</a>
+      <a class="active1" a href="<?php echo url('/contact') ?>">Contact</a>
+      <a href="#"><i class="fa fa-user 7x"></i></a>
+    </div>
+  </div>
+  <section>
+    <!--Formulaire de contact -->
+    <h2>Nous écrire</h2>
+    <form method="POST" action="contact/store">
+<!--question sur ce que mettre ds action-->
+      <div>
+        <label>
+          <span>Votre Nom</span>
+          <input type="text" name="nom" required placeholder="Entrez votre nom">
+        </label>
+      </div>
+      <div>
+        <label>
+          <span>Votre Email</span>
+          <input type="email" name="email" required placeholder="Entrez votre email">
+        </label>
+      </div>
+      <div>
+        <label>
+          <span>Votre Message</span>
+          <textarea name="message" cols="52" rows="5" required placeholder="Entrez votre message"></textarea>
+        </label>
+      </div>
+      <div class="button">
+        <button type="submit">Envoyez votre Message</button>
+      </div>
+      @csrf <!--champ input hidden pour laravel = sécurité -->
 
-
-
-
-    <nav>
-        <div class="menu-icon">
-           <a href="<?php echo url('/accueil') ?>"><img src="assets/images/logoa.png" class="logo2" alt="logo"></a> 
-            <div class="burger">
-            <span></span>
-
-            </div>
+      <!-- <div class="confirmation">
+            ICI AVEC AJAX, ON AFFICHERA LE MESSAGE DE CONFIRMATION 
         </div>
-
-        <div class="menu">
-            <ul>
-                <img src="assets/images/logoa.png" class="logoa" alt="logo">
-                <li><a class="active" a href="<?php echo url('/ecole') ?>">Écoles</a></li>
-                <li><a class="active1" a href="<?php echo url('/candidat') ?>">Candidats</a></li>
-                <li><a class="active2" a href="<?php echo url('/entreprise') ?>">Entreprises</a></li>
-                <li><a class="active3" a href="<?php echo url('/contact') ?>">Contact</a></li>
-                <li><a href="<?php echo url('/home') ?>"><i class="fa fa-user 7x" id="icon"></i></a></li>
-
-            </ul>
-        </div>
-
-    </nav>
-
-    <a id="myBtn" href="#accueil"><i class="fa fa-chevron-up" aria-hidden="true"></i></a>
-
-
-
-
-    <header id="accueil">
-        <div class="overlay">
-            <!-- <h1><span></span></h1> -->
-            <img src="assets/images/logo_italt_sombre.png" a href="#accueil" class="logo1" alt="logo">
-            <h2>LE PREMIER PAS VERS<br>L'ALTERNANCE</h2>
-
-    </header>
-
-
-
-    
-        <div class="title">
-            <h3><span>Que pouvons nous faire pour vous ?</span></h3>
-            <p>Cliquez sur la rubrique correspondant à votre profil pour découvrir ce que nous proposons</p>
-        </div>
-
-
-
-        <div id="imageBloc" style="text-align: center;">
-            <button id="entreprise">Vous êtes une Entreprise</button>
-            <button id="alternant">Vous êtes un Alternant</button>
-            <button id="ecole">Vous êtes une École</button>
-
-           <div class="alternant" style="display: block;"> 
-             <!-- <img src="assets/images/fond-card-alternant.png" class="carte" alt=""> -->
-             <p class='relative'><b>Prenez votre envol</b> <br><br>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
-                 labore et dolore magna aliqua. Ut enim ad minim veniam, 
-             quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. <br><br>
-             <a href="<?php echo url('/candidat') ?>"><button class="btn3">Commencer</button> </a> 
-             </p>
-              </div> 
-            
-            
-              <div class="ecole" style="display: none;"> 
-             <!-- <img src="assets/images/fond-card-ecole.png" class="carte" alt=""> -->
-              <p class='relative'><b>Trouvez vos apprenants</b> <br><br>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
-                 labore et dolore magna aliqua. Ut enim ad minim veniam, 
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. <br><br>
-              <a href="<?php echo url('/ecole') ?>"><button class="btn4">Commencer</button> </a> 
-              </p> 
-              </div>
-
-              <div class="entreprise" style="display:none;">  
-              <!-- <img src="assets/images/fond-card-entreprise.png" class="carte" alt=""> -->
-              <p class='relative'><b>Trouvez votre pépite</b> <br><br>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
-                 labore et dolore magna aliqua. Ut enim ad minim veniam, 
-                 quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. <br><br>
-              <a href="<?php echo url('/entreprise') ?>"><button class="btn5">Commencer</button> </a> 
-              </p> 
-              </div>
-           
-            <div class="Qui">
-                <img src="assets/images/qui.png" class=="qui1" alt="vecteur2">
-                <div class="center">
-                    <b>Qui sommes-nous ?</b>
-                    <br><br>Nous sommes une équipe de développeuses web FullStack et nous avons pour ambition de rendre
-                    plus facile l'accès à l'alternace dans le domaine du numérique. <br> C'est pourquoi la création de cette
-                    plateforme qui regrouperait les 3 parties et leurs besoins (Alternants, Entreprises, Écoles)
-                    , nous paraissait évidente.
-                </div>
-            </div>
-        </div>
-
-
-
-    <section id="contact">
-        <h3 class="insta">Ils nous soutiennent </h3>
-        <div class="figure-img1">
-            <a href=""><img src="assets/images/logo-part/logo-simplon.png" alt="instapic3"></a>
-            <a href=""><img src="assets/images/logo-part/logo-french-tech.png" alt="instapic2"></a>
-            <a href=""><img src="assets/images/logo-part/logo-marseille.png" alt="instapic1"></a>
-        </div>
-        <div class="Quii">
-            <img src="assets/images/orange.png" class="bandeorange" alt="vecteur2">
-            <div class="center1">
-                    <p class="action">Passez à l'action !</p>
-                    <button class="btn2" onclick=window.location.href='http://localhost/italt/italtequipe/public/register'>S'inscrire</button>
-                   </div>
-               </div>
-    </section>
-
-
-    <footer>
-        <div class="footer-container">
-
-            <a href="ml.html" target="_blank"></a>
-            <a href="licence.html" target="_blank"></a>
-           <!-- <p>Tous droits réservés - ©italt - 2019</p>-->
-
-            <!-- <div class="blockContact1">
-            <p class="blockContact">
-                    Téléphone : +33 7 82 19 76 43
-                    Email : <a href= "mailto:ITALT@gmail.com">Email</a>
-                    Facebook : <a href="">Facebook</a>
-                    Instagram : <a href="">Instagram</a>
-                    </p> -->
-        </div>
-        </div>
-    </footer>
-
-    <script src="<?php echo url('/assets/js/jquery.min.js') ?>"></script>
-    <script src="<?php echo url('/assets/js/sweetalert2.all.min.js') ?>"></script>
-    <script src="<?php echo url('/assets/js/main.js') ?>"></script>
+        INFO TECHNIQUE POUR DIFFERENCIER LES FORMULAIRES 
+        <input type="hidden" name="idFormulaire" value="contact">-->
+    </form>
+    <p>Nous répondrons dans un délai maximum de 4 jours</p>
+  </section>
+  <footer>
+  <ul>
+    <li> <a href="<?php echo url('/espace-admin') ?>">Admin</a> </li>
+  </ul>
+  </footer>
 </body>
 
 </html>
